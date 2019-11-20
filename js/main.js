3eb79135f006ba88aaa13f74d0b0ef821cb7e989
Philip Lundrigan <philiplundrigan@gmail.com>
2019-11-20 15:39:32 -0700

    Add Easter egg

--- js/main.js
+++ js/main.js
@@ -2,3 +2,14 @@ $(".abstract").click(function(event) {
     var description = $(event.target).parent().next();
     description.slideToggle("slow");
 });
+
+$("#the-net > a").click(function(event) {
+    if (event.ctrlKey || event.altKey || event.metaKey) {
+        console.log(`${event.ctrlKey} || ${event.altKey} || ${event.metaKey}`);
+        window.location.href = "https://www.imdb.com/title/tt0113957/";
+    }
+});
+
+// $("#background_image").mousemove(function( event ) {
+//   $("#background_image").css('background-position', event.pageX + 'px ' + event.pageY + 'px');
+// });
